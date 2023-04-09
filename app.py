e6335af10598daa0c8bc454a69e9552bb3c0c5f7
Jiwon Shin <JiwonShin@soongsil.ac.kr>
2023-04-10 02:13:42 +0900

    Add character rule register page

--- app.py
+++ app.py
@@ -5,42 +5,35 @@ import config
 import translate
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = config.flask_key
-app.config['UPLOAD_FOLDER'] = '*/Scripts/orig'
+app.config['SECRET_KEY'] = config.FLASK_KEY
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'ScriptEN')
+
+os.makedirs('ScriptEN', exist_ok=True)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+	return render_template('index.html')
 
 
-@app.route('/charRule', methods=['GET', 'POST'])
-def result():
-    if request.method == 'POST':
-        # Check if the post request has a file
-        if 'file' not in request.files:
-            flash("No file part")
-            return redirect(request.url)
-        
-        script = request.files["scriptFile"]
-        
-        # If file not uploaded
-        if script.filename == '':
-            flash("No file selected")
-            return redirect(request.url)
-        
-        # Save file in local
-        fname = secure_filename(script.filename)
-        script.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
-        print("File saved successfully")
+@app.route('/charRule', methods=['POST'])
+def charRegister():
+	if request.method == 'POST':
+		script = request.files['scriptFile']
 
-        # result<라는 딕셔너리를 받아서 넘겨주어야 함!!
-        print(os.path.join(app.config['UPLOAD_FOLDER'], fname))
-        result = translate.parseJson2Char(os.path.join(app.config['UPLOAD_FOLDER'], fname))
+		# If file is not empty
+		# if script.filename == '':
+		
+		# Save file in local
+		fname = secure_filename(script.filename)
+		script.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
+	
+	# result<라는 딕셔너리를 받아서 넘겨주어야 함!!
+	result = translate.parseJson2Char(os.path.join(app.config['UPLOAD_FOLDER'], fname))
 
-        #ttt = translate.getTranslation(script)
-        return render_template('ruleAppend.html', **locals())
+	#ttt = translate.getTranslation(script)
+	return render_template('ruleAppend.html', **locals())
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="8000", debug=True)
+	app.run(host="0.0.0.0", port="8000", debug=True)
--- translate.py
+++ translate.py
@@ -3,7 +3,7 @@ import json
 import openai
 import config
 
-openai.api_key = config.api_key
+openai.api_key = config.API_KEY
 
 
 ### Dictionaries ###
@@ -17,61 +17,68 @@ skill = dict()
 # Get names from json script and add into character dict
 # Returns character dict
 def parseJson2Char(filePath):
-    file = open(filePath)
-    data = json.load(file)
+	file = open(filePath)
+	data = json.load(file)
 
-    for id, script in data.items():
-        appendRule(character, script["NAME"], script["NAME"])
-    
-    return character
+	for id, script in data.items():
+		appendChar(script["NAME"], "")
+	
+	return character
 
 
 # Select dictionary, original word, translation word
 def appendRule(dic, origin, trans):
-    try:
-        dic[origin] = trans
-    except NameError:
-        print("Error! Selected dictionary doesn't exist")
-    return
+	try:
+		dic[origin] = trans
+	except NameError:
+		print("Error! Selected dictionary doesn't exist")
+	
+def appendChar(origin, trans):
+	character[origin] = trans
+	return character
+
+def appendReg(origin, trans):
+	character[origin] = trans
+	return region
 
 # Pre-translation with Translate Term Dictionary
 def ruleTrans(sentence):
-    # charcheck
-    if character:
-        for origin in character:
-            sentence = sentence.replace(origin, character[origin])
-    
-    # regcheck
-    if region:
-        for origin in region:
-            sentence = sentence.replace(origin, region[origin])
-            
-    # keycheck
-    if region:
-        for origin in keyword:
-            sentence = sentence.replace(origin, keyword[origin])
-    
-    # itemcheck
-    if item:
-        for origin in item:
-            sentence = sentence.replace(origin, item[origin])
-    
-    # skillcheck
-    if skill:
-        for origin in skill:
-            sentence = sentence.replace(origin, skill[origin])
-    
-    return sentence
+	# charcheck
+	if character:
+		for origin in character:
+			sentence = sentence.replace(origin, character[origin])
+	
+	# regcheck
+	if region:
+		for origin in region:
+			sentence = sentence.replace(origin, region[origin])
+					
+	# keycheck
+	if region:
+		for origin in keyword:
+			sentence = sentence.replace(origin, keyword[origin])
+	
+	# itemcheck
+	if item:
+		for origin in item:
+			sentence = sentence.replace(origin, item[origin])
+	
+	# skillcheck
+	if skill:
+		for origin in skill:
+			sentence = sentence.replace(origin, skill[origin])
+	
+	return sentence
 
 # Translate with GPT-3
 def getTranslation(script):
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="Translate this into Korean:" + script,
-        temperature=0.05,
-        max_tokens=500,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0)
-    
-    return response.choices[0].text.strip()
+	response = openai.Completion.create(
+		model="text-davinci-003",
+		prompt="Translate this into Korean:" + script,
+		temperature=0.05,
+		max_tokens=500,
+		top_p=1,
+		frequency_penalty=0,
+		presence_penalty=0)
+	
+	return response.choices[0].text.strip()
